b60a7daecee95678c3b04b06dc1c843c607b27a6
kawamataryo <ba068082@gmail.com>
2023-06-16 13:32:16 +0900

    🐛 fix ua

--- functions/src/clients/openAIClient.ts
+++ functions/src/clients/openAIClient.ts
@@ -33,6 +33,7 @@ export class OpenAIClient {
     const loader = new PuppeteerWebBaseLoader(url, {
       launchOptions: {
         headless: true,
+        args: ["--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"],
       },
       gotoOptions: {
         waitUntil: "domcontentloaded",
